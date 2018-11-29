e7f556f8dd064df9b19b4a42c780f7545f87a69e
o_0 <43157869+danger-academy@users.noreply.github.com>
2018-11-29 13:48:39 -0500

    start with pseudocode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,16 @@
+// Simple game, a timed trivia game where one multiple-choice question at a time is displayed, 
+// where the timer encompasses the entirety of the game,
+// and a final summary of correct/incorrect/unanswered questions is displayed at the end.
 
+// Start with the "start button"
+
+// Here we will define our questions and answers
+
+// Game mechanics will be set out here
+
+    // Countdown timer, display time remaining, announce when time is up
+	
+    // Game over, display final tally
+	
+        // Reset the game
+	
