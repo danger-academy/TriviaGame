4daefc7901b02510541668743632c89aa5401331
danger-academy <robert.tarkenton@gmail.com>
2019-01-04 21:36:37 -0500

    image update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,57 +16,57 @@ $(document).on('click', '#reset', function () {
     game.reset();
 })
 
-// Here we will define our questions and answers
+// Here we will define our questions and answers and corresponding images
 var questions = [{
-	question: "Which album was named by Magnet Magazine as the Best Album of the 90's",
-	answers: ["Radiohead ~ OK Computer", "Beck ~ Odelay", "Nirvana ~ Nevermind", "Neutral Milk Hotel ~ In The Aeroplane Over The Sea"],
-	correctAnswer: "Neutral Milk Hotel ~ In The Aeroplane Over The Sea",
-	image : "assets/images/aeroplane.jpg"
+    question: "Which album was named by Magnet Magazine as the Best Album of the 90's",
+    answers: ["Radiohead ~ OK Computer", "Beck ~ Odelay", "Nirvana ~ Nevermind", "Neutral Milk Hotel ~ In The Aeroplane Over The Sea"],
+    correctAnswer: "Neutral Milk Hotel ~ In The Aeroplane Over The Sea",
+    image: "./assets/images/aeroplane.jpg"
 }, {
-	question: "Which band or artist debuted in 1992 with the critically-acclaimed album, Dry?",
-	answers: ["PJ Harvey", "Bikini Kill", "Sleater-Kinney", "Nirvana"],
-	correctAnswer: "PJ Harvey",
-	image : "assets/images/pjharvey.jpg"
+    question: "Which band or artist debuted in 1992 with the critically-acclaimed album, Dry?",
+    answers: ["PJ Harvey", "Bikini Kill", "Sleater-Kinney", "Nirvana"],
+    correctAnswer: "PJ Harvey",
+    image: "./assets/images/pjharvey.jpg"
 }, {
-	question: "Which was the best record label of the 90's?",
-	answers: ["Nirvana", "4AD", "K Records", "Kill Rock Stars"],
-	correctAnswer: "Kill Rock Stars",
-	image : "assets/images/krs.jpg"
+    question: "Which was the best record label of the 90's?",
+    answers: ["Nirvana", "4AD", "K Records", "Kill Rock Stars"],
+    correctAnswer: "Kill Rock Stars",
+    image: "./assets/images/krs.jpg"
 }, {
-	question: "Which band formed to pay tribute to Mother Love Bone's late singer, Andrew Wood?",
-	answers: ["Alice in Chains", "Sublime", "Temple of the Dog", "Nirvana"],
-	correctAnswer: "Temple of the Dog",
-	image : "assets/images/temple.jpg"
+    question: "Which band formed to pay tribute to Mother Love Bone's late singer, Andrew Wood?",
+    answers: ["Alice in Chains", "Sublime", "Temple of the Dog", "Nirvana"],
+    correctAnswer: "Temple of the Dog",
+    image: "./assets/images/temple.jpg"
 }, {
-	question: "Which band or artist is widely credited with bringing trip hop to the mainstream, especially for US audiences?",
-	answers: ["Tone Loc", "Portishead", "Nirvana", "The Offspring"],
-	correctAnswer: "Portishead",
-	image : "assets/images/dummy.jpg"
+    question: "Which band or artist is widely credited with bringing trip hop to the mainstream, especially for US audiences?",
+    answers: ["Tone Loc", "Portishead", "Nirvana", "The Offspring"],
+    correctAnswer: "Portishead",
+    image: "./assets/images/portishead.png"
 }, {
-	question: "Which solo artist had a #1 hit single before even releasing a debut album?",
-	answers: ["Nirvana", "Tori Amos", "Courtney Love", "Lisa Loeb"],
-	correctAnswer: "Lisa Loeb",
-	image : "assets/images/loeb.jpg"
+    question: "Which solo artist had a #1 hit single before even releasing a debut album?",
+    answers: ["Nirvana", "Tori Amos", "Courtney Love", "Lisa Loeb"],
+    correctAnswer: "Lisa Loeb",
+    image: "./assets/images/loeb.jpg"
 }, {
-	question: "Which band formed in 1981, but didn't garner major attention until the release of the 1995 album, University?",
-	answers: ["Throwing Muses", "Sonic Youth", "Unwound", "Nirvana"],
-	correctAnswer: "Throwing Muses",
-	image : "assets/images/muses.jpg"
+    question: "Which band formed in 1981, but didn't garner major attention until the release of the 1995 album, University?",
+    answers: ["Throwing Muses", "Sonic Youth", "Unwound", "Nirvana"],
+    correctAnswer: "Throwing Muses",
+    image: "./assets/images/muses.jpg"
 }, {
-	question: "Which band formed in 1981, but in 1991 was given credit for 'breaking punk' by touring with bands such as Nirvana, Babes in Toyland, and Dinosaur Jr.?",
-	answers: ["Nirvana", "Sonic Youth", "Folk Implosion", "The Ramones"],
-	correctAnswer: "Sonic Youth",
-	image : "assets/images/sonic.jpg"
+    question: "Which band formed in 1981, but in 1991 was given credit for 'breaking punk' by touring with bands such as Nirvana, Babes in Toyland, and Dinosaur Jr.?",
+    answers: ["Nirvana", "Sonic Youth", "Folk Implosion", "The Ramones"],
+    correctAnswer: "Sonic Youth",
+    image: "./assets/images/sonic.jpg"
 }, {
-	question: "Which artist or band had the best-selling single of the 90's?",
-	answers: ["Pearl Jam ~ Evenflow", "Radiohead ~ Paranoid Android", "Elton John ~ Candle in the Wind", "Nirvana ~ Spank Thru"],
-	correctAnswer: "Elton John ~ Candle in the Wind",
-	image : "assets/images/elton.jpg"
+    question: "Which artist or band had the best-selling single of the 90's?",
+    answers: ["Pearl Jam ~ Evenflow", "Radiohead ~ Paranoid Android", "Elton John ~ Candle in the Wind", "Nirvana ~ Spank Thru"],
+    correctAnswer: "Elton John ~ Candle in the Wind",
+    image: "./assets/images/elton.png"
 }, {
-	question: "Which band, though thoroughly identified with the 90's, actually debuted in the 80's with their album, Bleach?",
-	answers: ["The Bee Gee's", "Aerosmith", "N*SYNC", "Nirvana"],
-	correctAnswer: "Nirvana",
-	image : "assets/images/nirvana.jpg"
+    question: "Which band, though thoroughly identified with the 90's, actually debuted in the 80's with their album, Bleach?",
+    answers: ["The Bee Gee's", "Aerosmith", "N*SYNC", "Nirvana"],
+    correctAnswer: "Nirvana",
+    image: "./assets/images/nirvana.jpg"
 }];
 // Game mechanics will be set out here
 var game = {
@@ -109,7 +109,8 @@ var game = {
         game.unanswered++;
         $('#content').html('<h2>Pencils down!</h2>')
         $('#content').append('<h3> The only acceptable answer was : ' + questions[game.
-            currentQuestion].correctAnswer + '</h3>');
+            currentQuestion].correctAnswer + '<br><img src=" ' + questions[game.
+                currentQuestion].image + ' ">' + '</h3>');
         if (game.currentQuestion == questions.length - 1) {
             setTimeout(game.results, 3 * 1000);
         } else {
@@ -138,20 +139,22 @@ var game = {
         console.log("You Got It");
         clearInterval(timer);
         game.correct++;
-        $('#content').html('<h2>Ooooooh, good for YOU.</h2>');
+        $('#content').html('<h2>Ooooooh, good for YOU. ' + '<br><img src=" ' + questions[game.
+            currentQuestion].image + ' ">' + '</h2>');
         if (game.currentQuestion == questions.length - 1) {
-            setTimeout(game.results, 2 * 1000);
+            setTimeout(game.results, 3 * 1000);
         } else {
-            setTimeout(game.nextQuestion, 2 * 1000);
+            setTimeout(game.nextQuestion, 3 * 1000);
         }
     },
     answeredIncorrectly: function () {
         console.log("Wrong!!!")
         clearInterval(timer);
         game.incorrect++;
-        $('#content').html('<h2>HA! Was that...was that a JOKE?</h2>');
+        $('#content').html('<h2>HA! Was that..was that a JOKE?</h2>');
         $('#content').append('<h3> The only acceptable answer was : ' + questions[game.
-            currentQuestion].correctAnswer + '</h3>');
+            currentQuestion].correctAnswer + '<br><img src=" ' + questions[game.
+                currentQuestion].image + ' ">' + '</h3>');
         if (game.currentQuestion == questions.length - 1) {
             setTimeout(game.results, 3 * 1000);
         } else {
